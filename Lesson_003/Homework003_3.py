63ecd9ee3db7e422305890e484cd2096188811b8
Anastasia <aml999@list.ru>
2022-12-04 00:44:39 +0300

    Add Lesson003

--- Lesson_003/Homework003_3.py
+++ Lesson_003/Homework003_3.py
@@ -5,15 +5,15 @@
 # [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
 size = 5
-frst = 1
+first = 1
 last = 9
 from random import uniform
-def get_numbers (size, frst, last):
-    return [round(uniform(frst,last), 2) for i in range(size)]
+def get_numbers (size, first, last):
+    return [round(uniform(first,last), 2) for i in range(size)]
 
 def find_diff(my_nums):
     nums = [round(x - int(x), 2) for x in (my_nums)]
     return max(nums) - min(nums)
-my_nums = get_numbers(size, frst, last)
+my_nums = get_numbers(size, first, last)
 print (my_nums)
 print(find_diff(my_nums))
