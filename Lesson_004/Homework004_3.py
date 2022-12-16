1bb1dfcacca55b12dcc4121948113b037e13a6de
Anastasia <aml999@list.ru>
2022-12-16 15:35:45 +0300

    Add lesson_004

---
+++ Lesson_004/Homework004_3.py
@@ -0,0 +1,19 @@
+# Задача 3.
+# Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
+
+from random import randint
+
+def new_list(size, m, n):
+    return [randint(m, n) for i in range(size)]
+
+def get_value(list):
+    return [i for i in set(list)]
+
+size = 5
+m = 1
+n = 10
+
+origin = new_list(size, m, n)
+print(origin)
+print(get_value(origin))
