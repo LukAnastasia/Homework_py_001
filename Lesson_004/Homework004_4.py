471f8c843403c54e15b46abfebc9f32db0eff726
Anastasia <aml999@list.ru>
2022-12-22 13:01:05 +0300

    Add lesson_004

--- Lesson_004/Homework004_4.py
+++ Lesson_004/Homework004_4.py
@@ -4,9 +4,74 @@
 # Пример:
 # k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
 
+
+# from random import randint
+# import itertools
+
+# k = randint(2, 2)
+
+# def get_ratios(k):
+#     ratios = [randint(0, 10) for i in range (k + 1)]
+#     while ratios[0] == 0:
+#         ratios[0] = randint(1, 10) 
+#     return ratios
+
+# def get_polynomial(k, ratios):
+#     var = ['*x']*(k-1) + ['*x']
+#     polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(ratios, var, range(k, 1, -1), fillvalue = '') if a !=0]
+#     for x in polynomial:
+#         x.append(' + ')
+#     polynomial = list(itertools.chain(*polynomial))
+#     polynomial[-1] = ' = 0'
+#     return "".join(map(str, polynomial)).replace(' 1*x',' x')
+
+
+# ratios = get_ratios(k)
+# polynom1 = get_polynomial(k, ratios)
+# print(polynom1)
+
+# with open('Polynomial.txt', 'w') as data:
+#     data.write(polynom1)
+
+    
 import random
+
+
+def write_file(st):
+    with open('Polynomial.txt', 'w') as data:
+        data.write(st)
+
 
 def rnd():
     return random.randint(0,101)
 
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x*{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
 k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
