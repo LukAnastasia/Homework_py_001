68e89e8df508e28a475004c005bef017154dfd12
Anastasia <aml999@list.ru>
2022-12-27 01:25:43 +0300

    Bugs fixed_004

--- Lesson_004/Homework004_5.py
+++ Lesson_004/Homework004_5.py
@@ -3,30 +3,71 @@
 # Задача - сформировать файл, содержащий сумму многочленов.
 
 import random
-import re
+koefs = {}
+# k = 15
+k = int(input('введите целое число больше 0: '))
+for i in range(k + 1):
+    koefs[i] = random.randint(1, 100)
+print(koefs)
+# {0: 71, 1: 62, 2: 44, 3: 89}
+# 89x^3+44x^2
+# print(max(koefs.keys()))
+max_k = max(koefs.keys())
+out_str = ''
+# c = 0
+first = True
+for i in range(max_k, -1, -1):
+    koef = koefs[i]
+    pow_x = i
+    if i > 1:
+        # if c == 0:
+        if first:
+            out_str += f'{koef}x^{pow_x}'
+            # 89x^3
+        else:
+            out_str += f'+{koef}x^{pow_x}'
+            # +89x^3
+    elif i == 1:
+        if first:
+            out_str += f'{koef}x'
+            # 62x
+        else:
+            out_str += f'+{koef}x'
+    else:
+        out_str += f'+{koef}=0'
+    # c += 1
+    if first:
+        first = False
+print(out_str)
+with open('result.txt', 'w', encoding='utf-8') as fd:
+    fd.write(out_str)
+    
 
-input_str = '54x^4-22x^3+34x^2+5x+18=0'
-pattern = r'([+-]?\d+)x\^?(\d*)|(\d*)=0$'
-# pattern = r'([-+]?\d*)(x\^*\d*)*'
-# pattern2 = r'([-+]?\d*)(x\^*\d*)*|(\=0)'
+# import random
+# import re
 
-# res = re.findall(pattern, input_str)
-# print(res)
-# re.compile()
-output = re.compile(pattern)
-result = output.findall(input_str)
-print(result)
-print(int(result[1][0]))
-res_dict = dict()
-for i in result:
-    if i[1]:
-        res_dict[int(i[1])] = int(i[0])
-    elif i[1] == '' and i[0]:
-        res_dict[1] = int(i[0])
-    elif i[2]:
-        res_dict[0] = int(i[2])
-print(res_dict)
-# '54x^4-22x^3+34x^2+5x+18=0'
+# input_str = '54x^4-22x^3+34x^2+5x+18=0'
+# pattern = r'([+-]?\d+)x\^?(\d*)|(\d*)=0$'
+# # pattern = r'([-+]?\d*)(x\^*\d*)*'
+# # pattern2 = r'([-+]?\d*)(x\^*\d*)*|(\=0)'
+
+# # res = re.findall(pattern, input_str)
+# # print(res)
+# # re.compile()
+# output = re.compile(pattern)
+# result = output.findall(input_str)
+# print(result)
+# print(int(result[1][0]))
+# res_dict = dict()
+# for i in result:
+#     if i[1]:
+#         res_dict[int(i[1])] = int(i[0])
+#     elif i[1] == '' and i[0]:
+#         res_dict[1] = int(i[0])
+#     elif i[2]:
+#         res_dict[0] = int(i[2])
+# print(res_dict)
+# # '54x^4-22x^3+34x^2+5x+18=0'
 # k = 3
 # res_dict = dict()
 # res_dict[2] = random.randint(0, 100)
---
+++ Lesson_007/Homework007.py
@@ -0,0 +1,86 @@
+# Задание: Создать телефонный справочник с возможностью импорта и экспорта данных в нескольких форматах.
+
+
+filename = "phonebook.txt" 
+myfile = open(filename, "a+") 
+myfile.close 
+ 
+# главное меню
+def main_menu(): 
+    print( "\nТелефонный справочник") 
+    print( "\nВажно!!! Данные нужно вводить буквами латинского алфавита!!!\n") 
+    print( "1. Показать весь список контактов") 
+    print( "2. Добавить новый контакт") 
+    print( "3. Найти в списке контактов") 
+    print( "4. Выход") 
+    choice = input("Введите ID нужной функции и нажмите Enter: ") 
+    if choice == "1": 
+        myfile = open(filename, "r+") 
+        filecontents = myfile.read() 
+        if len(filecontents) == 0: 
+            print( "Вы ввели неверные данные") 
+        else: 
+            print(filecontents) 
+        myfile.close 
+        enter = input("Нажмите Enter для продолжения ...") 
+        main_menu() 
+    elif choice == "2": 
+        newcontact() 
+        enter = input("Нажмите Enter для продолжения ...") 
+        main_menu() 
+    elif choice == "3": 
+        searchcontact() 
+        enter = input("Нажмите Enter для продолжения ...") 
+        main_menu() 
+    elif choice == "4": 
+        print("Спасибо, что воспользовались телефонным справочником!") 
+    else: 
+        print( "Вы ввели некорректные данные!\n") 
+        enter = input( "Нажмите Enter для продолжения ...") 
+        main_menu() 
+ 
+# поиск       
+def searchcontact(): 
+    searchname = input( "Введите имя для поиска в телефонной книге: ") 
+    remname = searchname[1:] 
+    firstchar = searchname[0] 
+    searchname = firstchar.upper() + remname 
+    myfile = open(filename, "r+") 
+    filecontents = myfile.readlines() 
+
+      
+    found = False 
+    for line in filecontents: 
+        if searchname in line: 
+            print( "Резельтат поиска:", end = " ") 
+            print( line) 
+            found = True 
+            break 
+    if found == False: 
+        print( "Контакт " + searchname + " не найден") 
+ 
+# вводим имя и фамилию
+def input_firstname(): 
+    first = input( "Введите имя: ") 
+    remfname = first[1:] 
+    firstchar = first[0] 
+    return firstchar.upper() + remfname 
+ 
+def input_lastname(): 
+    last = input( "Введите фамилию: ") 
+    remlname = last[1:] 
+    firstchar = last[0] 
+    return firstchar.upper() + remlname 
+ 
+# добавить данные
+def newcontact(): 
+    firstname = input_firstname() 
+    lastname = input_lastname() 
+    phoneNum = input( "Введите номер телефона: ") 
+    emailID = input( "Введите E-mail: ") 
+    contactDetails =("[" + firstname + " " + lastname + ", " + phoneNum + ", " + emailID +  "]\n") 
+    myfile = open(filename, "a") 
+    myfile.write(contactDetails) 
+    print( "\nДанные:\n " + contactDetails + "\nбыли успешно сохранены!\n") 
+ 
+main_menu()
